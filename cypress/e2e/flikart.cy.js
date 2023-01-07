379eccf2dbbd8d1b288b27699ffc9c8bccb7d778
Vinay <vinaykumarg0591@gmail.com>
2023-01-07 22:56:57 +0530

    thirdcommit

--- cypress/e2e/flikart.cy.js
+++ cypress/e2e/flikart.cy.js
@@ -8,6 +8,8 @@ it('Flikart Login', ()=>{
     cy.visit('https://www.flipkart.com/')
     cy.get('._3704LK').type('Iphone 12 Pro Max')
     cy.get('.col-12-12 _1MRYA1').find('Iphone 12').click()
+    cy.go('back')
+    cy.go('forward')
 
 
 
